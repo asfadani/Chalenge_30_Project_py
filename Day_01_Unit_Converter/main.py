e2a30223ea780358e0a9617c3ba19f0c0825043e
asfadani <asfa.dani0001@gmail.com>
2025-12-17 18:11:52 +0700

    Membuat menu awal program konversi suhu

---
+++ Day_01_Unit_Converter/main.py
@@ -0,0 +1,13 @@
+
+def menu_utama():
+    print(f"""
+    {"Selamat Datang di Program Konversi Suhu".center(47)}
+    {"="*47}
+    {"Suhu dalam 0 drajat Celcius".center(47)}
+    Fahrenheit = 32     Kelvin = 273    Reamur = 0
+    {"="*47}
+    {"Suhu yang akan di konversi".center(47)}
+    {"(Celcius/Fahrenheit/Kelvin/Reamur)".center(47)}
+    """)
+
+menu_utama()

6eecd9cf22a6910cd3ad5d69cfb8c6068eaa1ee6
asfadani <asfa.dani0001@gmail.com>
2025-12-24 10:58:46 +0700

    membuat package pribadi

---
+++ library_pribadi/__init__.py
@@ -0,0 +1,8 @@
+import os
+print("library pribadi telah berhasil dimuat!")
+
+if os.name == 'nt':
+    os.system('cls')
+else:
+    os.system('clear')
+
---
+++ library_pribadi/ui.py
@@ -0,0 +1,10 @@
+import os
+
+def clear_screen():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def kembali():
+    input("\nTekan ENTER untuk kembali ke menu utama...")

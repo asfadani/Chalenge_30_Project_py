2296107e359f75568094821452d9310fcf5a5109
asfadani <asfa.dani0001@gmail.com>
2025-12-28 08:07:05 +0700

    meringkas impor package

--- Day_03_Password_Manage_Generator/setup.py
+++ Day_03_Password_Manage_Generator/setup.py
@@ -2,3 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from library_pribadi import ui
+from password_generator import PasswordGenerator
+
+("Print package terload")
+ui.clear_screen()

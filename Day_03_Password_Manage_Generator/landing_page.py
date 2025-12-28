8c4fcc3354a8c646b5b38e583726d0595d2a5f17
asfadani <asfa.dani0001@gmail.com>
2025-12-28 08:08:21 +0700

    landing_page_final program

--- Day_03_Password_Manage_Generator/landing_page.py
+++ Day_03_Password_Manage_Generator/landing_page.py
@@ -1,7 +1,5 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from library_pribadi import ui
+from setup import ui
+import main_program
 
 admin = {}
 
@@ -17,29 +15,42 @@ def tambah_admin():
 
 while True:
     print(f"{"SELAMAT DATANG DI PROGRAM PASSWORD MANAGER".center(50)}\n{"="*50}")
-
     if not admin:
-        print(f"{"Belum ada admin terdaftar, daftar admin".center(50)}")
+        print(f"{"Belum ada admin terdaftar, silahkan daftar admin".center(50)}\n{"(Username dan password tidak boleh hanya huruf q->quit)".center(50)}")
         tambah_admin()
         ui.kembali()
+        ui.clear_screen()
         continue
     else:
-            try:
-                print(f"{"Login".center(50)}")
-                usn = input("Masukkan username : ")
-                pw = input("Masukkan password : ")
+        try:
+            print(f"{"Login".center(50)}")
+            usn = input("Masukkan username : ")
+            pw = input("Masukkan password : ")
 
-                if usn == admin['Username'] and pw == admin["Password"]:
-                    ui.clear_screen()
-                    print("menuju menu utama")
-                    break
-                else:
-                    print("Username atau Password salah")
-                    ui.kembali
-                    ui.clear_screen
-                    continue
-            except:
-                print("input tidak sesuai")
+            if usn == admin['Username'] and pw == admin["Password"]:
+                ui.clear_screen()
+                main_program.program()
+            elif usn == "q" and pw == "q":
+                ui.clear_screen()
+                print("TERIMAKASIH TELAH MENGGUNAKAN PROGRAM SEDERHANA PASWORD MANAJEMEN")
+                break
+            elif usn != admin['Username']:
+                print("\nUsername salah")
                 ui.kembali()
                 ui.clear_screen()
                 continue
+            elif pw != admin["Password"]:
+                print("\nPassword salah")
+                ui.kembali()
+                ui.clear_screen()
+                continue
+            elif usn != admin['Username'] and pw != admin["Password"]:
+                print("\nUsername dan password salah")
+                ui.kembali()
+                ui.clear_screen()
+                continue
+        except Exception as e:
+            print(f"\nTerjadi error: {e}")
+            print(f"Tipe error: {type(e)}")
+            print("Silakan ulangi input data.\n")
+            continue

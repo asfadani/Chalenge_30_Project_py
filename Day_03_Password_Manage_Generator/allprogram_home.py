6574eba78a3f4e8c31828eb04dc1b6c52a1d8738
asfadani <asfa.dani0001@gmail.com>
2025-12-28 08:06:03 +0700

    logika program_final

--- Day_03_Password_Manage_Generator/allprogram_home.py
+++ Day_03_Password_Manage_Generator/allprogram_home.py
@@ -1,10 +1,5 @@
-import random
-from re import template
-from password_generator import PasswordGenerator
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from library_pribadi import ui
+from setup import PasswordGenerator
+from setup import ui
 
 tempalte_akunmanager = {
     'Akun' : "akun",
@@ -13,30 +8,18 @@ tempalte_akunmanager = {
 
 daftar = {}
 
-def tampilkan_daftar():
-    print(f"{"Aplikasi":<15} {"Akun":<15} {"Password":<15} \n{"="*50}")
-    if not daftar:
-        print("Tidak ada data terdaftar")
-    else:
-        for key in daftar:
-            Akun = daftar[key]['Akun']
-            Password = daftar[key]['Password']
-
-        print(f"{key:<15} {Akun:<15} {Password:<15}")
 
 def kecuali():
-    kecuali = input("Kecualikan Upper/Lower/Number/Char (y/n)?").lower()
+    kecuali = input("Kecualikan Upper/Lower/Number/Char (y/n)? ").lower()
     if kecuali != "y":
         return
     else:
-        print("TEKAN ENTER UNTUK MELEWATI")
+        print("\nTEKAN ENTER UNTUK MELEWATI")
         kapital = input("huruf kapital(upper) yang ingin dikecualikan : ").upper()
         lower = input("Huruf non-kapital(lower) yang ingin dikecualikan : ").lower()
         angka = input("Angka yang ingin dikecualikan : ")
         schar = input("Karakter spesial yang ingin dikecualikan : ")
-        return kapital, lower, angka, schar
-
-    
+        return kapital, lower, angka, schar    
 def generator(panjang, kapital, lower, angka, schar):
     pwg = PasswordGenerator()
     pwg.maxlen = panjang
@@ -47,85 +30,112 @@ def generator(panjang, kapital, lower, angka, schar):
     password = pwg.generate()
     return password
     
-def generator_password():
-    while True:
-        akun = dict.fromkeys(tempalte_akunmanager.keys())
-        try:
-            aplikasi = input("Masukkan nama aplikasi : ")
-            akunnya = input("Masukkan alamat akun")
-            panjangnya = int(input("Panjang password : "))
-            kecualikan = kecuali()
-        except Exception as e:
-            print(f"Terjadi error: {e}")
-            print(f"Tipe error: {type(e)}")
-        
-        akun['Akun'] = akunnya
-        pw = generator(panjangnya, kecualikan)
-        akun['Password'] = pw
 
-        count = 0
-        if aplikasi is not daftar:
-            daftar.update({aplikasi:akun})
+class Fitur():
+    @staticmethod
+    def tampilkan_daftar():
+        print(f"{"Aplikasi":<15} {"Akun":<20} {"Password":<15} \n{"="*50}")
+        if not daftar:
+            print("Tidak ada data terdaftar")
         else:
-            aplikasi = f"{aplikasi}{count}"
+            for key in daftar:
+                Akun = daftar[key]['Akun']
+                Password = daftar[key]['Password']
+                print(f"{key:<15} {Akun:<20} {Password:<15}")
+        
+    @staticmethod
+    def generator_password():
+        while True:
+            akun = dict.fromkeys(tempalte_akunmanager.keys())
+            try:
+                aplikasi = input("Masukkan nama aplikasi : ")
+                akunnya = input("Masukkan alamat akun : ")
+                panjang = int(input("Panjang password : "))
+                hasil_kecuali = kecuali()
+                if hasil_kecuali:
+                    pw = generator(panjang, *hasil_kecuali)
+                else:
+                    pw = generator(panjang, "", "", "", "")
+            except Exception as e:
+                print(f"\nTerjadi error: {e}")
+                print(f"Tipe error: {type(e)}")
+                print("Silakan ulangi input data.\n")
+                continue
+        
+            akun['Akun'] = akunnya
+            akun['Password'] = pw
+
+            count = 0
+            key_aplikasi = aplikasi
+            if aplikasi in daftar:
+                aplikasi = f"{aplikasi}{count}"
+                daftar.update({aplikasi:akun})
+                count += 1
             daftar.update({aplikasi:akun})
-            count += 1
    
-        for key, value in daftar.items():
-            print("Data telah terdaftar")
-            print(f"{key} \t: {value}")
+            for key, value in daftar.items():
+                print("Data telah terdaftar")
+                print(f"{key} \t: {value}")
 
-        sudah = input("\nTambah daftar lagi (y/n)? ").lower()
-        if sudah != "y":
-            break
+            sudah = input("\nTambah daftar lagi (y/n)? ").lower()
+            if sudah != "y":
+                break
+    @staticmethod
+    def update_delete():
+        Fitur.tampilkan_daftar()
+        while True:
+            tanya_lagi = False
+            try:
+                ubah = input("\nApa yang ingin anda lakukan (update/delete/back) ? ").lower()
+                if ubah in ["delete", "del", "d"]:
+                    hapus = input("Masukkan nama aplikasi yang ingin dihapus : ")
+                    if hapus in daftar:
+                        del daftar[hapus]
+                        print(f"\nData aplikasi {hapus} berhasil dihapus")
+                        Fitur.tampilkan_daftar()
+                        tanya_lagi = True
+                    else:
+                        print("\nData tidak ditemukan.")
+                        continue
 
-def update_delete():
-    tampilkan_daftar()
-    while True:
-        try:
-            ubah = input("Apa yang ingin anda lakukan (update/delete) ? ").lower()
-            if ubah == ("delete" or "del" or "d"):
-                hapus = input("Masukkan kata kunci(aplikasi/akun) yang ingin dihapus : ")
-            elif ubah == ("update" or "up" or "u"):
-                perbarui = input("Masukkan kata kunci(aplikasi/akun) yang ingin diupdate : ")
-            else:
-                print("Input tidak sesuai")
+                elif ubah in ["update", "up", "u"]:
+                    opsi = input("Apa yang ingin diupdate (aplikasi/akun)? ").lower()
+                    if opsi == "aplikasi":
+                        target = input("Masukkan nama aplikasi lama: ")
+                        if target in daftar:
+                            baru = input(f"Perbarui aplikasi {target} menjadi : ")
+                            data_tersimpan = daftar.pop(target)
+                            daftar[baru] = data_tersimpan
+                            print(f"\nAplikasi {target} berhasil diupdate menjadi {baru}")
+                            Fitur.tampilkan_daftar()
+                            tanya_lagi = True
+                        else:
+                            print(f"\nData aplikasi {baru} tidak ditemukan")
+                            continue
+                    elif opsi == "akun":
+                        app_akun = input("Masukkan nama aplikasi pemilik akun: ")
+                        if app_akun in daftar:
+                            akun_baru = input(f"Ubah akun {daftar[app_akun]['Akun']} menjadi : ")
+                            daftar[app_akun]['Akun'] = akun_baru
+                            print("\nAkun berhasil diperbarui")
+                            tanya_lagi = True
+                        else:
+                            print(f"\nData aplikasi {baru} tidak ditemukan")
+
+                elif ubah in ["back", "b"]:
+                    break
+                else:
+                    print("\nInput tidak sesuai")
+                    continue
+            except Exception as e:
+                print(f"\nTerjadi error: {e}")
+                print(f"Tipe error: {type(e)}")
                 continue
 
-        except Exception as e:
-            print(f"Terjadi error: {e}")
-            print(f"Tipe error: {type(e)}")
-        
-        tanya_lagi = False
-
-        if hapus or perbarui in daftar.items():
-            if hapus:
-                del daftar[hapus]
-                print(f"Data dengan kata kunci {hapus} berhasil dihapus")
-                tampilkan_daftar()
-                tanya_lagi = True
-            elif perbarui:
-                if perbarui in daftar.keys():
-                    baru = input(f"Perbarui aplikasi {perbarui} menjadi : ")
-                    daftar[perbarui] = baru
-                    print(f"Aplikasi {perbarui} berhasil diupdate menjadi {baru}")
-                    tampilkan_daftar()
-                    tanya_lagi = True
-                elif perbarui in daftar.values():
-                    baru = input(f"Perbarui akun {perbarui} menjadi : ")
-                    daftar[perbarui] = baru
-                    print(f"Akun {perbarui} berhasil diupdate menjadi {baru}")
-                    tampilkan_daftar()
-                    tanya_lagi = True
-            else:
-                print(f"Data {perbarui} tidak ditemukan di daftar")
-                tanya_lagi = True
-
-        if tanya_lagi == True:
-            pilihan = input("Update/delete data lain(y/n)? ")
-            if pilihan != "y":
-                break
-
+            if tanya_lagi == True:
+                pilihan = input("\nUpdate/delete data lain(y/n)? ")
+                if pilihan != "y":
+                    break
 
 
 

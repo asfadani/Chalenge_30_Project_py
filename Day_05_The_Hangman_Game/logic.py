ca3a409b05023da01d73f6585b42dc53f50ebbd5
asfadani <asfa.dani0001@gmail.com>
2026-01-04 08:18:55 +0700

    logic 1.2

--- Day_05_The_Hangman_Game/logic.py
+++ Day_05_The_Hangman_Game/logic.py
@@ -1,4 +1,8 @@
 import random
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from library_pribadi import ui
 
 kata_mudah = ["BUKU", "MEJA", "BOLA", "SAPI", "BIRU", 
     "AIR",  "API",  "TAS",  "JAM",  "ROTI", 
@@ -44,6 +48,35 @@ def kesulitan(input_kesulitan):
     else:
         return None
 
+def tampilkan_kesulitan(input_kesulitan):
+    if input_kesulitan in ["mudah", "m"]:
+        return "Mudah"
+    elif input_kesulitan in ["normal", "n"]:
+        return "Normal"
+    elif input_kesulitan in ["sulit", "s"]:
+        return "Sulit"
+    elif input_kesulitan in ["random", "r"]:
+        return "Random"
+    else:
+        return None 
+
+# def sudah_ditebak(input_jwb):
+#     if not input_jwb:
+#         return None
+#     else:
+#         return input_jwb[-1]
+
+def pesan(input_jwb, jawaban):
+    if not input_jwb:
+        print("Belum ada jawaban dikirim")
+    else:
+        terakhir = input_jwb[-1]
+        if (len(terakhir) == 1 and terakhir in jawaban) or (terakhir == jawaban):
+            print(f"Huruf/kata {terakhir} ada pada jawaban")
+        else:
+            print(f"Sayang sekali, huruf/kata {terakhir} salah!")
+
+
 def tampilan_jawaban(kata_jawaban):
     global kata_rahasia
     if not kata_rahasia:
@@ -54,44 +87,50 @@ def tampilan_jawaban(kata_jawaban):
     
 
 def mencocokkan_huruf(jawaban_user, jawaban):
-    list(jawaban_user)
-    while jawaban_user in jawaban:
+    global kata_rahasia
+    if jawaban_user in jawaban:
         for i, huruf in enumerate(jawaban):
             if jawaban_user == huruf:
-                return i
-        if len(jawaban_user) > 1:
-            for j, hrf_jwban in enumerate(jawaban_user):
-                if hrf_jwban in jawaban:
-                    return j
+                kata_rahasia[i] = jawaban_user
+        return kata_rahasia
+    if len(jawaban_user) > 1:
+        for index, huruf_asli in enumerate (jawaban):
+            if huruf_asli in jawaban_user:
+                kata_rahasia[index] = huruf_asli
+        return kata_rahasia
     return None
 
-def reveal(indek_sama, jawaban_user):
-    while indek_sama is not None:
-        kata_rahasia[indek_sama] = jawaban_user
-    return
+def menu():
+    print(f"""
+{'='*50}
+{'SELAMAT DATANG DI PERMAINAN THE HANGING GAME'.center(50)}
+{'='*50}
+""")
 
-level = input("pilih kesulitan(m,n,s,r) : ")
-lvl = kesulitan(level)
+# if __name__ == "__main":
+#     level = input("pilih kesulitan(m,n,s,r) : ")
+#     lvl = kesulitan(level)
 
-count = 5
-while count > 0:
-    tampilan_jawaban(lvl)
-    print("Kata : ")
-    for i in kata_rahasia:
-        print(i, end="")
+# count = 5
+# while count > 0:
+#     tampilan_jawaban(lvl)
+#     print("Kata : ")
+#     for i in kata_rahasia:
+#         print(i, end="")
     
-    print("\nINFO")
-    print(f"Kesempatan \t: {count}")
-    jwb_usr = input("Masukkan Huruf jawaban : ").upper()
+#     print("\nINFO")
+#     print(f"Kesempatan \t: {count}")
+#     jwb_usr = input("Masukkan Huruf jawaban : ").upper()
 
-    hsl = mencocokkan_huruf(jwb_usr, lvl)
-    reveal(hsl, jwb_usr)
-    count -= 1
+#     mencocokkan_1huruf(jwb_usr, lvl)
+#     count -= 1
 
-if count == 0:
-    print(f"anda kalah, jawabannya {lvl}")
+# if count == 0:
+#     print(f"anda kalah, jawabannya {lvl}")
 
-
+# sdh = ["j"]
+# jwb = "API"
+# pesan(sdh, jwb)
 
 
 

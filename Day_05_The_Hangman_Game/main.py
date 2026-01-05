9ec12fc5fe8d23d56a0b2f307a8dab7f41151578
asfadani <asfa.dani0001@gmail.com>
2026-01-05 13:50:56 +0700

    main 1.1

--- Day_05_The_Hangman_Game/main.py
+++ Day_05_The_Hangman_Game/main.py
@@ -3,6 +3,7 @@ import logic
 
 
 while True:
+    logic.ui.clear_screen()
     kata_rahasia = []
     sudah = []
     logic.menu()
@@ -14,28 +15,33 @@ while True:
     logic.ui.clear_screen()
     count = 5
     while count > 0:
-        logic.tampilan_jawaban(lvl)
+        kt_rhs = logic.tampilan_jawaban(lvl)
+        if not kata_rahasia:
+            kata_rahasia.extend(kt_rhs)
         print(f"Level\t: {logic.tampilkan_kesulitan(level)}")
-        print("Kata:")
-        for i in kata_rahasia:
-            print(i, end="")
+        print(f"Kata\t: {kata_rahasia}")
+
         
         print("\n[INFO]")
-        print(f"> Sisa Kesempatan \t: {count}")
-        print(f">Huruf/Kata yang sudah ditebak : {sudah}")
+        print(f"> Sisa Kesempatan \t\t: {count}")
+        print(f"> Huruf/Kata yang sudah ditebak : {sudah}")
         logic.pesan(sudah, lvl)
         jwb_usr = input("\nMasukkan huruf/kata jawaban : ").upper()
+        if jwb_usr in kata_rahasia or jwb_usr in sudah:
+            print("Anda sudah menjawab pilihan tersebut")
+            continue
         sudah.append(jwb_usr)
-
-        logic.mencocokkan_huruf(jwb_usr, lvl)
+        if "_" not in kata_rahasia:
+            logic.ui.clear_screen()
+            print(f"{'='*50}\n{'SELAMAT ANDA MENANG!'.center(50)}\n\njawabannya\t: {lvl}\nJawaban anda\t: {kata_rahasia}\nTrack jawaban\t: {sudah}\n{'='*50}")
+            break
+        logic.mencocokkan_huruf(jwb_usr, lvl, kata_rahasia)
         count -= 1
 
-    if count == 0 and "-" in kata_rahasia:
-        print(f"{'='*50}{'\nSayang sekali anda kalah, jawabannya'.center(50)}\n{lvl}\n{'='*50}")
-        break
-    if "_" not in kata_rahasia:
-        print(f"{'='*50}{'\nSayang sekali anda kalah, jawabannya'.center(50)}\n{lvl}\n{'='*50}")
-        break
+    if count == 0 and "_" in kata_rahasia:
+        logic.ui.clear_screen()
+        print(f"{'='*50}\n{'Sayang sekali anda kalah'.center(50)}\n\njawabannya\t: {lvl}\nJawaban anda\t: {kata_rahasia}\nTrack jawaban\t: {sudah}\n{'='*50}")
+
 
     if logic.ui.lagi():
         logic.ui.clear_screen()

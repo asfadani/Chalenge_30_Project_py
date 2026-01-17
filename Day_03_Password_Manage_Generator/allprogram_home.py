7b0317eeaf00e6c8dab83dba26255394c44b056a
asfadani <asfa.dani0001@gmail.com>
2026-01-17 13:27:52 +0700

    memeperbarui file program home dengan konfigurasi penyimpanan lokal

--- Day_03_Password_Manage_Generator/allprogram_home.py
+++ Day_03_Password_Manage_Generator/allprogram_home.py
@@ -1,13 +1,9 @@
 from setup import PasswordGenerator
 from setup import ui
+from setup import penyimpanan
 
-tempalte_akunmanager = {
-    'Akun' : "akun",
-    'Password' : "password"
-}
 
-daftar = {}
-
+data_akun = penyimpanan.load_csv()
 
 def kecuali():
     kecuali = input("Kecualikan Upper/Lower/Number/Char (y/n)? ").lower()
@@ -20,10 +16,11 @@ def kecuali():
         angka = input("Angka yang ingin dikecualikan : ")
         schar = input("Karakter spesial yang ingin dikecualikan : ")
         return kapital, lower, angka, schar    
+    
 def generator(panjang, kapital, lower, angka, schar):
     pwg = PasswordGenerator()
     pwg.maxlen = panjang
-    pwg.minlen = 1
+    pwg.minlen = panjang
     pwg.excludeuchars = kapital
     pwg.excludelchars = lower
     pwg.excludenumbers = angka
@@ -36,13 +33,15 @@ class Fitur():
     @staticmethod
     def tampilkan_daftar():
         print(f"{'Aplikasi':<15} {'Akun':<20} {'Password':<15} \n{'='*50}")
-        if not daftar:
+        if len(data_akun) == 0:
             print("Tidak ada data terdaftar")
         else:
-            for key in daftar:
-                Akun = daftar[key]['Akun']
-                Password = daftar[key]['Password']
-                print(f"{key:<15} {Akun:<20} {Password:<15}")
+            for key in data_akun:
+                aplikasi = key["Aplikasi"]
+                akun = key["Akun"]
+                password = key["Password"]
+
+                print(f"{aplikasi:<15} {akun:<20} {password:<15}")
         
     @staticmethod
     def generator_password():
@@ -62,28 +61,19 @@ class Fitur():
                 print(f"Tipe error: {type(e)}")
                 print("Silakan ulangi input data.\n")
                 continue
-                
-            akun_data = {
-                'Akun' : akunnya,
-                'Password' : pw
-            }   
             
             count = 1
             key_aplikasi = aplikasi
-            while key_aplikasi in daftar:
+            while ui.ada_tidak(key_aplikasi, data_akun):
                 key_aplikasi = f"{aplikasi}_{count}"
                 count += 1
-            if key_aplikasi != aplikasi:
-                print(f"Info: Nama '{aplikasi}' sudah ada. Disimpan sebagai '{key_aplikasi}'")
-            daftar[key_aplikasi] = akun_data
-   
-            for key, value in daftar.items():
-                print("Data telah terdaftar")
-                print(f"{key} \t: {value}")
+            data_akun.append({"Aplikasi": key_aplikasi, "Akun":akunnya, "Password": pw })
+            penyimpanan.save_csv(data_akun)
+            print("Data telah terdaftar")
 
-            sudah = input("\nTambah daftar lagi (y/n)? ").lower()
-            if sudah != "y":
+            if ui.selesai():
                 break
+
     @staticmethod
     def update_delete():
         Fitur.tampilkan_daftar()
@@ -92,40 +82,30 @@ class Fitur():
             try:
                 ubah = input("\nApa yang ingin anda lakukan (update/delete/back) ? ").lower()
                 if ubah in ["delete", "del", "d"]:
-                    hapus = input("Masukkan nama aplikasi yang ingin dihapus : ")
-                    if hapus in daftar:
-                        del daftar[hapus]
-                        print(f"\nData aplikasi {hapus} berhasil dihapus")
-                        Fitur.tampilkan_daftar()
-                        tanya_lagi = True
-                    else:
-                        print("\nData tidak ditemukan.")
+                    try:
+                        HAPUS = input("\nMasukkan nama aplikasi yang akan dihapus: ")
+                        index = ui.cari_index(HAPUS, data_akun)
+                        del data_akun[index]
+                        penyimpanan.save_csv(data_akun)
+                        print(f"Data {HAPUS} berhasil terhapus")
+                    except Exception as e:
+                        print(f"\nTerjadi error: {e}")
+                        print(f"Tipe error: {type(e)}")
                         continue
-
                 elif ubah in ["update", "up", "u"]:
-                    opsi = input("Apa yang ingin diupdate (aplikasi/akun)? ").lower()
-                    if opsi == "aplikasi":
-                        target = input("Masukkan nama aplikasi lama: ")
-                        if target in daftar:
-                            baru = input(f"Perbarui aplikasi {target} menjadi : ")
-                            data_tersimpan = daftar.pop(target)
-                            daftar[baru] = data_tersimpan
-                            print(f"\nAplikasi {target} berhasil diupdate menjadi {baru}")
-                            Fitur.tampilkan_daftar()
-                            tanya_lagi = True
-                        else:
-                            print(f"\nData aplikasi {baru} tidak ditemukan")
-                            continue
-                    elif opsi == "akun":
-                        app_akun = input("Masukkan nama aplikasi pemilik akun: ")
-                        if app_akun in daftar:
-                            akun_baru = input(f"Ubah akun {daftar[app_akun]['Akun']} menjadi : ")
-                            daftar[app_akun]['Akun'] = akun_baru
-                            print("\nAkun berhasil diperbarui")
-                            tanya_lagi = True
-                        else:
-                            print(f"\nData aplikasi {app_akun} tidak ditemukan")
-
+                    try:
+                        pilih = int(input("\nData ke berapa yang akan dirubah: "))
+                        index = pilih - 1
+                        opsi = input("masukkan data yang ingin diubah: ")
+                        key = ui.cari_key(opsi, data_akun[index])
+                        baru = input("Masukkan data baru: ")
+                        data_akun[index][key] = baru
+                        penyimpanan.save_csv(data_akun)
+                        print(f"Data {opsi} berhasil dirubah menjadi, {baru}")
+                    except Exception as e:
+                        print(f"\nTerjadi error: {e}")
+                        print(f"Tipe error: {type(e)}")
+                        continue
                 elif ubah in ["back", "b"]:
                     break
                 else:
@@ -136,10 +116,8 @@ class Fitur():
                 print(f"Tipe error: {type(e)}")
                 continue
 
-            if tanya_lagi == True:
-                pilihan = input("\nUpdate/delete data lain(y/n)? ")
-                if pilihan != "y":
-                    break
+            if ui.selesai():
+                break
 
 
 

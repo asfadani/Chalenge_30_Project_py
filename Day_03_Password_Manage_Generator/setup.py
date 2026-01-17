5319fb0408e62f1d9f17ad1203af43c5d4d73441
asfadani <asfa.dani0001@gmail.com>
2026-01-17 13:26:42 +0700

    menambahkan file penyimpanan ke setup

--- Day_03_Password_Manage_Generator/setup.py
+++ Day_03_Password_Manage_Generator/setup.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import penyimpanan
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from library_pribadi import ui
 from password_generator import PasswordGenerator

e46c0e695c9d1c014be7537e95502acf2d8da3c0
asfadani <asfa.dani0001@gmail.com>
2026-02-02 10:39:10 +0700

    membuat database dari dataframe file csv

--- Day_09_Pipeline/save_db.py
+++ Day_09_Pipeline/save_db.py
@@ -4,7 +4,14 @@ import os
 
 
 folder = os.path.dirname(os.path.abspath(__file__))
+file_csv =os.path.join(folder, 'marketing_warehouse.csv')
+df = pd.read_csv(file_csv)
+print(df.head())
+
+
+folder = os.path.dirname(os.path.abspath(__file__))
 file_database =os.path.join(folder, 'marketing_warehouse.db')
+
 
 try:
     koneksi = sqlite3.connect(file_database)
@@ -29,8 +36,18 @@ kursor.execute('''
 
 print("Database dan tabel berhasil dibuat")
 
+# for i, row in df.iterrows():
+#     sql = "INSERT INTO prospek (nama, umur, gender, nat, email) VALUES (?,?,?,?,?)"
+#     kursor.execute(sql, (row['nama'], row['umur'], row['gender'], row['nat'], row['email']))
 
-
+# kursor.execute("DELETE FROM prospek")
+# kursor.execute("DELETE FROM sqlite_sequence WHERE name='prospek'")
 
 koneksi.commit()
+print("\nMenampilkan data dari database")
+kursor.execute("SELECT * FROM prospek")
+baris_data = kursor.fetchall()
+for data in baris_data:
+    print(data[:5])
+
 kursor.close()
